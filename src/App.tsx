034293ccd5f5203c62f6fca27a9b10e35909f124
Irine7 <irine7@yandex.ru>
2025-11-09 17:46:42 +0100

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { Button, Icon, Layout } from "@stellar/design-system";
+import "./App.module.css";
+import ConnectAccount from "./components/ConnectAccount.tsx";
+import { Routes, Route, Outlet, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import Debugger from "./pages/Debugger.tsx";
+import Test from './pages/Test.tsx';
+
+const AppLayout: React.FC = () => (
+  <main>
+    <Layout.Header
+      projectId="Stellar Tea"
+      projectTitle="Stellar Tea"
+      contentRight={
+        <>
+          <nav>
+            <NavLink
+              to="/debug"
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              {({ isActive }) => (
+                <Button
+                  variant="tertiary"
+                  size="md"
+                  onClick={() => (window.location.href = "/debug")}
+                  disabled={isActive}
+                >
+                  <Icon.Code02 size="md" />
+                  Debugger
+                </Button>
+              )}
+            </NavLink>
+          </nav>
+          <ConnectAccount />
+        </>
+      }
+    />
+    <Outlet />
+    <Layout.Footer>
+      <span>
+        © {new Date().getFullYear()} Stellar Tea. Licensed under the{" "}
+        <a
+          href="http://www.apache.org/licenses/LICENSE-2.0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Apache License, Version 2.0
+        </a>
+        .
+      </span>
+    </Layout.Footer>
+  </main>
+);
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/debug" element={<Debugger />} />
+        <Route path="/debug/:contractName" element={<Debugger />} />
+        <Route path="/test" element={<Test />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
